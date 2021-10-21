cf73a32117264768fa91f1cede6164c05f6c9642
Nnasy <omi.t.ab@m.titech.ac.jp>
2021-10-21 14:03:54 +0900

    main作成

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 minsup = 2 #input("minsup:")
 
 #a~g → 1~7で表現  _は0
@@ -8,10 +6,4 @@ S2 = [ [1,2], [3], [2,3], [1,5] ]
 S3 = [ [5,6], [1,2], [4,6], [3], [2] ]
 S4 = [ [5], [7], [1,6], [3], [2], [3] ]
 DB = [S1, S2, S3, S4]
-
-
-def postfix(a,b):
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            if a[i][j] == b[len(a)]:
 
