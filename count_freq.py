2852e50f8ca7c57823824a7fafbfd8b09453524e
Nnasy <omi.t.ab@m.titech.ac.jp>
2021-10-21 20:00:15 +0900

    postfix一次完成

--- count_freq.py
+++ count_freq.py
@@ -3,6 +3,7 @@ import numpy as np
 
 minsup = 2 #input("minsup:")#
 
+#a~g → 1~7で表現  _は0
 S1 = [ [1], [1,2,3], [1,3], [4], [3,6] ]#
 S2 = [ [1,4], [3], [2,3], [1,5] ]#
 S3 = [ [5,6], [1,2], [4,6], [3], [2] ]#
@@ -10,33 +11,24 @@ S4 = [ [5], [7], [1,6], [3], [2], [3] ]#
 DB = [S1, S2, S3, S4]#
 
 counts = [[1,2,3,4,5,6,7],
-          [0,0,0,0,0,0,0]]
+          [0,0,0,0,0,0,0]]#入力値によって生成するように要変更
 
+#def count_freq(DB,freq_pattern):#freq_patternは頻度をカウントしたい系列パターン配列
+
+
+
+#登場頻度カウント
 for i in range(len(DB)):
     ary = list(itertools.chain.from_iterable(DB[i]))
     for j in range(len(counts[0])):
         if j+1 in ary:
             counts[1][j] += 1
-print(list(itertools.chain.from_iterable(DB[0])))
-print(counts)
+#print(list(itertools.chain.from_iterable(DB[0])))
+#print(counts)
 
+#登場頻度がminsup未満の要素の検索
 del_elements = []
 for i in range(len(counts[0])):
     if counts[1][i] < minsup:
         del_elements.append(counts[0][i])
-print(del_elements)
-
-def del_by_freq(del_element, pattern):
-    output = []
-    for i in range(len(pattern)):
-        output.append([])
-        for j in range(len(pattern[i])):
-            if pattern[i][j] != del_element:
-                output[i].append(pattern[i][j])
-    for i in range(len(output)):
-        if not output[len(output)-i-1]:
-            del output[len(output)-i-1]
-    return output
-
-print(S4)
-print(del_by_freq(7, S4))
+#print(del_elements)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from del_by_freq import del_by_freq
+from del_by_freq import del_by_freq
 minsup = 2 #input("minsup:")
 
 #a~g → 1~7で表現  _は0
@@ -7,3 +9,12 @@ S3 = [ [5,6], [1,2], [4,6], [3], [2] ]
 S4 = [ [5], [7], [1,6], [3], [2], [3] ]
 DB = [S1, S2, S3, S4]
 
+
+
+
+
+#登場頻度がminsup未満の要素の削除
+#DB[3] = del_by_freq(7, DB[3])
+for i in range(len(del_elements)):
+    for j in range(len(DB)):
+        DB[j] = del_elem_in_ptn(del_elements[i], DB[j])
